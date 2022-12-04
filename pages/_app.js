2e9c510b0d7a1227a7ecdfe1b2cdb8fda7ba24d4
delesslin <delesslingw@gmail.com>
2022-12-04 18:52:03 -0500

    :rocket: initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import '../styles/reset.css'
+import '../styles/variables.css'
+import '../styles/layout.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div id='body'>
+      <header>Header</header>
+      <main>
+        <Component {...pageProps} />
+      </main>
+      <footer>
+        Developed and maintained by a nepotistic, capitalist cabal.
+      </footer>
+    </div>
+  )
 }
 
 export default MyApp
